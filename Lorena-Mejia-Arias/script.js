27fb7d1a56fac98e0a7839b62471736412641777
Lorena Mejia Arias <lorena.mejia@globant.com>
2026-01-10 14:31:46 -0500

    Ejercicio 1 , implementacion Tecnica de conceptos de Programacion Declarativa (Sin terminar)

---
+++ Lorena-Mejia-Arias/script.js
@@ -0,0 +1,40 @@
+// EJERCICIO 1
+
+/*1) Metodo Inmutable y mutables*/
+
+/* Metodos Mutables: aquel que puede cambiar su contenido original cuando se usa un metodo sobre el
+Push(): Agrega uno o más elementos al final del arreglo,sirve para añadir datos nuevos a una lista existente.
+Pop(): Elimina el último elemento del arreglo, sirve para quitar el último dato ingresado.
+splice(): Agrega, elimina o reemplaza elementos en cualquier posición sirve para modificar el contenido interno del arreglo.
+
+ Metodos Inmutables: no modifica un arreglo original, si no que retorna uno nuevo
+ map(): Transforma cada elemento y devuelve un nuevo arreglo sirve para cuando necesitas modificar los datos sin tocar el original.
+ filter(): Devuelve solo los elementos que cumplen una condición, sirve para seleccionar datos específicos.
+ slice(): Extrae una parte del arreglo sin modificarlo.
+ 
+ 2) Programación Imperativa vs Declarativa
+
+ Programación Imperativa: es el “como hacerlo” este indica paso a paso lo que debe hacer el programa.
+
+ Programación Declarativa: es el “que se quiere” este indica que describe el resultado esperado, no el proceso.
+ 
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ==================================================== //
+
